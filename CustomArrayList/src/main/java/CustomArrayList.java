135e1d8173a59464f0c0b62430e84c5bbe5eb228
aleksandr-kolobov <forward2010@inbox.ru>
2023-11-12 22:55:56 +0700

    Add some methods

--- CustomArrayList/src/main/java/CustomArrayList.java
+++ CustomArrayList/src/main/java/CustomArrayList.java
@@ -1,26 +1,121 @@
 package main.java;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 
 public class CustomArrayList<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final Object[] EMPTY_LIST = {};
     private Object[] dataArray;
-    private int size;
+    private int size = 0;
 
+    public CustomArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.dataArray = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.dataArray = EMPTY_LIST;
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+        }
+    }
 
+    public CustomArrayList() {
+        this.dataArray = EMPTY_LIST;
+    }
+
+    public CustomArrayList(Collection<? extends T> c) {
+        Object[] a = c.toArray();
+        if ((size = a.length) != 0) {
+            dataArray = Arrays.copyOf(a, size, Object[].class);
+        } else {
+            dataArray = EMPTY_LIST;
+        }
+    }
+
+    private Object[] grow(int minCapacity) {
+        int oldCapacity = dataArray.length;
+        if (oldCapacity > 0 || dataArray != EMPTY_LIST) {
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            return dataArray = Arrays.copyOf(dataArray, newCapacity);
+        } else {
+            return dataArray = new Object[Math.max(DEFAULT_CAPACITY, minCapacity)];
+        }
+    }
 
   /*
- - add(int index, E element)
- - addAll(Collection<? extends E> c)
- - clear()
- - get(int index)
- - isEmpty()
+ + add(E element)
+ + add(int index, E element)
+ + addAll(Collection<? extends E> c)
+ + clear()
+ + get(int index)
+ + size()
+ + isEmpty()
  - remove(int index)
  - remove(Object o)
- - sort(Comparator<? super E> c)
+ + sort(Comparator<? super E> c)
   */
 
+    private void add(T e, Object[] elementData, int s) {
+        if (s == elementData.length) {
+            elementData = grow(s + 1);
+        }
+        elementData[s] = e;
+        size = s + 1;
+    }
+
+    public void add(T e) {
+        add(e, dataArray, size);
+    }
+
+    public void add(int index, T element) {
+        checkIndex(index);
+        int s = size;
+        Object[] elementData = this.dataArray;
+        if (s == elementData.length) {
+            elementData = grow(s + 1);
+        }
+        System.arraycopy(elementData, index, elementData, index + 1, s - index);
+        elementData[index] = element;
+        size += 1;
+    }
+
+    public boolean addAll(Collection<? extends T> collection) {
+        Object[] array = collection.toArray();
+        int numNew = array.length;
+        if (numNew == 0) {
+            return false;
+        }
+        int s = size;
+        Object[] elementData = this.dataArray;
+        if (numNew > (elementData.length - s)) {
+            elementData = grow(s + numNew);
+        }
+        System.arraycopy(array, 0, elementData, size, numNew);
+        size += numNew;
+        return true;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            dataArray[i] = null;
+        }
+        size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        checkIndex(index);
+        return (T) dataArray[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
 
     @SuppressWarnings("unchecked")
     public void sort(Comparator<? super T> comparator) {
@@ -60,4 +155,11 @@ public class CustomArrayList<T> {
             quickSort(array, begin, rightCursor, comparator);
         }
     }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+    }
+
 }
