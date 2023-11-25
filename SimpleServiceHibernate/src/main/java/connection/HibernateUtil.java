79bf7c0a4f4571d066ad3d4e29120b1c68d8cf54
aleksandr-kolobov <forward2010@inbox.ru>
2023-11-25 20:14:26 +0700

    add synhronized

--- SimpleServiceHibernate/src/main/java/connection/HibernateUtil.java
+++ SimpleServiceHibernate/src/main/java/connection/HibernateUtil.java
@@ -9,7 +9,7 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-    private static SessionFactory sessionFactory = null;
+    private static synchronized SessionFactory sessionFactory = null;
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
