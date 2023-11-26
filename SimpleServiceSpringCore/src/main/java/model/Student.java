9b937cd68c1a9fe7c12da217aab22614f37f5010
aleksandr-kolobov <forward2010@inbox.ru>
2023-11-26 19:57:37 +0700

    перенес предыдущую работу в новую ветку

---
+++ SimpleServiceSpringCore/src/main/java/model/Student.java
@@ -0,0 +1,30 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "students")
+public class Student implements Comparable<Student> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "student_to_course",
+            joinColumns = {@JoinColumn(name = "student_id")},
+            inverseJoinColumns = {@JoinColumn(name = "course_id")})
+    private List<Course> courses;
+
+    @Override
+    public int compareTo(Student s) {
+        return id.compareTo(s.getId());
+    }
+}
---
+++ SimpleServiceSpringCore/src/main/java/service/CourseServiceImpl.java
@@ -0,0 +1,45 @@
+package service;
+
+import model.Course;
+import model.Student;
+import model.Teacher;
+import repository.CourseRepository;
+import repository.CourseRepositoryImpl;
+
+import java.util.List;
+
+public class CourseServiceImpl implements CourseService {
+
+    private final CourseRepository courseRepository = new CourseRepositoryImpl();
+
+    @Override
+    public Course findById(Integer id) {
+        return courseRepository.findById(id);
+    }
+
+    @Override
+    public void save(Course course) {
+        courseRepository.save(course);
+    }
+
+    @Override
+    public void update(Course course) {
+        courseRepository.update(course);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        courseRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Course> findAll() {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public List<Teacher> findAllTeachersOfCourse(Integer courseId) {
+        return courseRepository.findAllTeachersOfCourse(courseId);
+    }
+
+}
