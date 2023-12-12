f54c04ffe894f6caadec26bc3674b8ae67ec1546
aleksandr-kolobov <forward2010@inbox.ru>
2023-12-12 13:20:30 +0700

    Разложу на две папки два проекта на gradl

--- SimpleServiceSpringBoot/simple-web/src/main/java/org/alexkolo/simpleweb/exception/TaskNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.alexkolo.simpleweb.exception;
-
-public class TaskNotFoundException extends RuntimeException{
-
-    public TaskNotFoundException() {
-        throw new RuntimeException("Task not found");
-    }
-
-}
